ab478f67bb644a96161ba6d1ca5cbce0b465d751
Viet Le <leanhviet24495@gmail.com>
2021-12-04 12:00:19 +0700

    Update app file

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const routes = require('./routes')
+const routes = require('./src/routes')
 const bodyParser = require('body-parser')
 const path = require('path')
 require('dotenv').config()
